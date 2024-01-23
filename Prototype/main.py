477931af0dc4b1799fa1f558b5786d64a2ce299b
torphage <filip.torphage1@gmail.com>
2024-01-23 14:32:15 +0100

    Improved compressed product + cleaned up code
    Co-authored-by: luksch1121 <luksch1121@users.noreply.github.com>

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,45 +1,38 @@
 import numpy as np
-import numpy.typing as npt
 import scipy as sp
-from typing import NamedTuple
 
 
-class Hashes(NamedTuple):
-    h1: npt.NDArray
-    h2: npt.NDArray
-    s1: npt.NDArray
-    s2: npt.NDArray
-
-
-def init_arrays(n, b, d, rng):
+def init_arrays(b, d, n, rng):
+    # All the variables that are declared here are initiated as 2d matrices.
     p = np.zeros((d, b), dtype="complex_")
     h1, h2 = rng.integers(0, b, (d, n)), rng.integers(0, b, (d, n))
     s1, s2 = rng.choice([-1, 1], (d, n)), rng.choice([-1, 1], (d, n))
     return (p, h1, h2, s1, s2)
 
 
-def compressed_product(m1, m2, b, d, rng, n):
-    p, h1, h2, s1, s2 = init_arrays(n, b, d, rng)
+def compressed_product(m1, m2, b, d, n, rng):
+    p, h1, h2, s1, s2 = init_arrays(b, d, n, rng)
     for t in range(d):
         for k in range(n):
-            pa, pb = np.zeros(b), np.zeros(b)
+            pa, pb = np.zeros(b, dtype="complex_"), np.zeros(b, dtype="complex_")
+
             for i in range(n):
                 pa[h1[t, i]] += s1[t, i] * m1[i, k]
+
             for j in range(n):
                 pb[h2[t, j]] += s2[t, j] * m2[k, j]
-            pa, pb = np.fft.fft(pa), np.fft.fft(pb)
 
-            for z in range(b):
-                p[t, z] += pa[z] * pb[z] # Casting complex valu
-    for t in range(d):
-        p[t] = np.fft.ifft(p[t])
+            pa, pb = np.fft.fft(pa), np.fft.fft(pb)
+            p[t] += np.multiply(pa, pb)
+
+    p = np.fft.ifft(p, axis=-1)
     return (p, h1, h2, s1, s2)
 
 
-def recover_element(p, h1, h2, s1, s2, b, d, i, j):
+def decompress_element(p, h1, h2, s1, s2, b, d, i, j):
     xt = np.zeros(d)
     for t in range(d):
-        xt[t] = s1[t, i] * s2[t, j] * p[t, (h1[t, i] + h2[t, j]) % b]
+        xt[t] = s1[t, i] * s2[t, j] * np.real(p[t, (h1[t, i] + h2[t, j]) % b])
     return np.median(xt)
 
 
@@ -47,35 +40,85 @@ def decompress_matrix(p, h1, h2, s1, s2, b, d, n):
     c = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
-            c[i, j] = recover_element(p, h1, h2, s1, s2, b, d, i, j)
+            c[i, j] = decompress_element(p, h1, h2, s1, s2, b, d, i, j)
     return c
 
 
-def main(seed=None):
-    n = 12
-    d = 15
-    b = 5
-
-    random1 = sp.sparse.random(12, 12, density=0.05, random_state=seed).A
-    random2 = sp.sparse.random(12, 12, density=0.05, random_state=seed).A
-
-    rng = np.random.default_rng(seed=seed)
-
-    # pretty_print_compressed((p,h1,h2,s1,s2))
-    print((random1 * random2).round(3))
-    print()
-    p, h1, h2, s1, s2 = compressed_product(random1, random2, b, d, rng, n)
-
-    # print(compressed_product(m1, m2, b, d, rng, n))
+def calculate_result(m1, m2, b, d, n, rng):
+    p, h1, h2, s1, s2 = compressed_product(m1, m2, b, d, n, rng)
     return decompress_matrix(p, h1, h2, s1, s2, b, d, n)
 
 
-def pretty_print_compressed(c):
-    print("")
-    print(f"p:  {c[0].round(3)}")
-    print("")
+def random_sparse_matrix(n, density, rng):
+    """
+    Generates a random sparse n×n matrix with the
+    option to specify the density of non-zero elements.
+    """
+    return sp.sparse.random(n, n, density=density, random_state=rng).A
+
+
+def pretty_print_matrix(mat, n):
+    """
+    Pretty prints matrices such that any two matrices lines up when printing them
+    """
+    rounded_digit = 3
+    if n > 31:
+        # These are the parts of the matrix that will be displayed
+        q1 = mat[:3, :3]  # top left
+        q2 = mat[:3, n - 3 :]  # top right
+        q3 = mat[n - 3 :, :3]  # bottom left
+        q4 = mat[n - 3 :, n - 3 :]  # bottom right
+
+        # if any displayed digits are negative, then round the matrix by of less
+        if np.any((q1 < 0) | (q2 < 0) | (q3 < 0) | (q4 < 0)):
+            rounded_digit = 2
+    else:
+        if np.any(mat < 0):
+            rounded_digit = 2
+
+    print(mat.round(rounded_digit))
+
+
+def setup(seed=None):
+    # Disables scientific notation for floats
+    np.set_printoptions(suppress=True)
+
+    if not seed:
+        seed = int(np.random.rand() * (2**32 - 1))
+        print(f"seed: {seed}")
+    return np.random.default_rng(seed=seed)
 
 
 if __name__ == "__main__":
-    result = main(seed=1984)
-    print(result.round(2))
+    rng = setup(seed=1337)  # Set seed here to reproduce results
+
+    # user variables
+    n = 3  # The size of the matrix
+    matrix_A = random_sparse_matrix(n, 0.45, rng)  # Can also hard code a matrix here
+    matrix_B = random_sparse_matrix(n, 0.45, rng)  # Can also hard code a matrix here
+    b = 7
+    d = 6
+
+    # Calculate the final matrix product
+    p, h1, h2, s1, s2 = compressed_product(matrix_A, matrix_B, b, d, n, rng)
+    result = decompress_matrix(p, h1, h2, s1, s2, b, d, n)
+
+    # This rounds all the elements in the result matrix to the 15th decimal.
+    # This is used to offset Python's floating point precision problem.
+    # It's used when counting the number of non-zero elements.
+    rounded_res = result.round(12)
+    print("\n------ Compressed Matrix Product ------")
+    print(f"Non-zero elements: {np.count_nonzero(rounded_res)} of {n*n}")
+    print(f"Sum of elements: {np.sum(result)}")
+    rounded_res[rounded_res == 0.0] = 0.0  # Converts -0 to 0 (they're equivalent)
+    print("Product:")
+    pretty_print_matrix(rounded_res, n)
+
+    print("\n--------- Real Matrix Product ---------")
+    real_product = matrix_A * matrix_B
+    r = real_product.round(12)
+    print(f"Non-zero elements: {np.count_nonzero(r)} of {n*n}")
+    print(f"Sum of elements: {np.sum(real_product)}")
+    r[r == 0.0] = 0.0  # Converts -0 to 0 (they're equivalent)
+    print("Product:")
+    pretty_print_matrix(r, n)
