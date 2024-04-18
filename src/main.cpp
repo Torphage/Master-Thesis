e166d58ff5aeeee7e9d1db3f98c01fef725e42f4
torphage <filip.torphage1@gmail.com>
2024-04-18 21:23:44 +0200

    updated main using newest (best?) compress version

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main() {
     int b = 50, d = 5, n = 10;
 
-    unsigned int seed = 5; // std::random_device{}();
+    unsigned int seed = 5;  // std::random_device{}();
     std::mt19937_64 rng(seed);
     std::uniform_real_distribution<float> uni(-1.0, 1.0);
 
@@ -25,21 +25,21 @@ int main() {
     int threads = omp_get_max_threads();
     MatrixRXd compressed = MatrixRXd::Zero(d, b);
     MatrixRXd pas = MatrixRXd::Zero(threads, b);
-    MatrixRXd pbs = MatrixRXd::Zero(threads, b);
+    // MatrixRXd pbs = MatrixRXd::Zero(threads, b);
     MatrixRXcd ps = MatrixRXcd::Zero(d, b / 2 + 1);
     ArrayRXcd sum = ArrayRXcd::Zero(b / 2 + 1);
     MatrixRXcd out1(threads, b / 2 + 1);
     MatrixRXcd out2(threads, b / 2 + 1);
     fft_struct fft1 = init_fft(b, pas.data(), out1.data());
-    fft_struct fft2 = init_fft(b, pbs.data(), out2.data());
+    fft_struct fft2 = init_fft(b, pas.data(), out2.data());
     ifft_struct ifft = init_ifft(b, ps.data(), compressed.data());
 
     MatrixRXd result = MatrixRXd::Zero(n, n);
-    MatrixRXd xt = MatrixRXd::Zero(n, d);
+    MatrixRXd xt = MatrixRXd::Zero(threads, d);
 
-    bompressed_product_par_deluxe_threaded(m1, m2, b, d, hash, compressed, pas, pbs, ps, sum, out1, out2, fft1, fft2, ifft);
+    bompressed_product_par_deluxe_special_edition(m1, m2, b, d, hash, compressed, pas, ps, out1, out2, fft1, fft2, ifft);
 
-    debompress_matrix_par(compressed, n, b, d, hash, result, xt);
+    debompress_matrix_par_threaded(compressed, n, b, d, hash, result, xt);
 
     clean_fft(fft1);
     clean_fft(fft2);
